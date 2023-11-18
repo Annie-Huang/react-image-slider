fec3283686993c75bb1ed0da340215203fbe4d52
Annie Huang <annie.hn@gmail.com>
2023-11-19 10:55:42 +1100

    Create a IMAGES array

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import car1 from './imgs/car-1.jpg';
+import car2 from './imgs/car-2.jpg';
+import car3 from './imgs/car-3.jpg';
+import car4 from './imgs/car-4.jpg';
+import car5 from './imgs/car-5.jpg';
+
+const IMAGES = [car1, car2, car3, car4, car5];
+
 function App() {
   return <h1> Hi</h1>;
 }
