ac7dc440c68e1ef740e477f05cf3d6b5a894ab91
Annie Huang <annie.hn@gmail.com>
2023-11-19 13:26:31 +1100

    Put to the correct position

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -10,10 +10,10 @@ export const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
       <img src={imageUrls[imageIndex]} className='img-slider-img' />
-      <button className='img-slider-btn'>
+      <button className='img-slider-btn' style={{ left: 0 }}>
         <ArrowBigLeft />
       </button>
-      <button className='img-slider-btn'>
+      <button className='img-slider-btn' style={{ right: 0 }}>
         <ArrowBigRight />
       </button>
     </div>
