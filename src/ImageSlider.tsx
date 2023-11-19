ffc69a63785c11133acd74a9602a3fb72b9d0e41
Annie Huang <annie.hn@gmail.com>
2023-11-19 15:19:05 +1100

    add 'aria-hidden' to all the images that is not currently display

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -31,11 +31,12 @@ export const ImageSlider = ({ images }: ImageSliderProps) => {
           overflow: 'hidden',
         }}
       >
-        {images.map(({ url, alt }) => (
+        {images.map(({ url, alt }, index) => (
           <img
             key={url}
             src={url}
             alt={alt}
+            aria-hidden={imageIndex !== index}
             className='img-slider-img'
             style={{ translate: `${-100 * imageIndex}%` }}
           />
